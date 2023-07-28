bfe77a7ecdef6ed26a4b0c74c56dcfdf66d37e6a
joren97x <babamyloves2002@gmail.com>
2023-07-28 16:57:28 +0800

    updates

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: '/todo-list/`',
+  publicPath: '/todo-list/',
 
   pluginOptions: {
     vuetify: {
