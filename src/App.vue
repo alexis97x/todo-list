109669513736420c59e2d999ff1b5f75d5719509
joren97x <babamyloves2002@gmail.com>
2023-07-28 14:16:23 +0800

    responsive

--- src/App.vue
+++ src/App.vue
@@ -45,85 +45,119 @@
 <template>
     <v-app>
         <v-main>
-            <v-container class="text-center w-50">
-                <v-container>
-                    <h4>
-                        <i>{{quote}}</i>
-                    </h4>
-                    <p class="text-end mr-4">
-                        {{ '-'+author }}
-                    </p>
+            <v-container class="text-center">
+                <v-container class="w-100">
+                    <v-container>
+                        <h4>
+                            <i>{{quote}}</i>
+                        </h4>
+                        <p class="text-end mr-4">
+                            {{ '-'+author }}
+                        </p>
+                    </v-container>
+                    <v-text-field
+                        v-model="newTask"
+                        @keydown.enter="create"
+                        label="What are you working on?"
+                    >
+                        <template v-slot:append-inner>
+                            <v-fade-transition>
+                                <v-tooltip text="Creates a new task">
+                                    <template v-slot:activator="{ props }">
+                                        <v-btn
+                                            icon="mdi-plus"
+                                            size="x-small"
+                                            v-bind="props"
+                                            v-show="newTask"
+                                            @click="create"
+                                        ></v-btn>
+                                    </template>
+                                </v-tooltip>
+                            </v-fade-transition>
+                        </template>
+                    </v-text-field>
+                    <!-- <h1>Tasks: 
+                        <v-fade-transition leave-absolute>
+                            <span :key="`${tasks.length}`">
+                                {{tasks.length}}
+                            </span>
+                        </v-fade-transition>
+                    </h1> -->
+                    <v-divider />
+                    <v-row class="mt-3">
+                        <v-col
+                            cols="12"
+                            sm="6"
+                            md="4"
+                            class="my-2"
+                        >
+                            Remaining: 
+                            <v-fade-transition leave-absolute>
+                                <span :key="remainingTasks" class="text-red">
+                                    {{ remainingTasks }}
+                                </span>
+                            </v-fade-transition>
+                        </v-col>
+                        <v-divider vertical class="my-2" />
+                        <v-col
+                            cols="12"
+                            sm="6"
+                            md="4"
+                            class="my-2"
+                        >
+                            Completed: 
+                            <v-fade-transition leave-absolute>
+                                <span :key="completedTasks" class="text-success">
+                                    {{ completedTasks }}
+                                </span>
+                            </v-fade-transition>
+                        </v-col>
+                        <v-col
+                            cols="12"
+                            sm="12"
+                            md="4"
+                        >
+                            <v-progress-circular
+                                v-show="tasks.length > 0"
+                                class="me-3 mt-2"
+                                color="success"
+                                :model-value="(completedTasks / tasks.length) * 100 "
+                            >
+                            </v-progress-circular>
+                        </v-col>
+                    </v-row>
+                    <v-divider class="mt-5" />
+
+                    <v-card class="mt-4" v-show="tasks.length > 0">
+                        <v-slide-y-transition group>
+                            <div v-for="task in tasks" :key="task.task_id">
+                                <v-hover v-slot="{isHovering, props}">
+                                    <v-divider />
+                                    <v-list-item :class="isHovering ? 'bg-grey-lighten-3' : ''" v-bind="props">
+                                        <v-checkbox-btn
+                                            @click="completeTask(task)"
+                                            :label="task.name"
+                                            :class="task.is_completed ? 'text-grey' : 'text-black'"
+                                        >
+                                        </v-checkbox-btn>
+                                        <template v-slot:append>
+                                            <v-fade-transition>
+                                                <v-icon
+                                                    color="success"
+                                                    v-show="task.is_completed"
+                                                >
+                                                    mdi-check
+                                                </v-icon>
+                                            </v-fade-transition>
+                                        </template>
+                                    </v-list-item>
+                                </v-hover>
+                            </div>
+                        </v-slide-y-transition>
+                    </v-card>
                 </v-container>
-                <v-text-field
-                    v-model="newTask"
-                    @keydown.enter="create"
-                    label="What are you working on?">
-                
-                    <template v-slot:append-inner>
-                        <v-fade-transition>
-                            <v-tooltip text="Creates a new task">
-                                <template v-slot:activator="{ props }">
-                                    <v-btn icon="mdi-plus" size="x-small" v-bind="props" v-show="newTask" @click="create" ></v-btn>
-                                </template>
-                            </v-tooltip>
-                        </v-fade-transition>
-                    </template>
-                
-                </v-text-field>
-                <!-- <h1>Tasks: 
-                    <v-fade-transition leave-absolute>
-                        <span :key="`${tasks.length}`">
-                            {{tasks.length}}
-                        </span>
-                    </v-fade-transition>
-                </h1> -->
-                <v-divider/>
-                <v-row class="mt-3">
-                    <v-col-3 class="mx-3">
-                        Remaining: 
-                        <v-fade-transition leave-absolute>
-                            <span :key="remainingTasks" class="text-red">
-                                {{ remainingTasks }}
-                            </span>
-                        </v-fade-transition>
-                    </v-col-3>
-                    <v-divider vertical/>
-                    <v-col-3 class="ms-3">
-                        Completed: 
-                        <v-fade-transition leave-absolute>
-                            <span :key="completedTasks" class="text-success">
-                                {{ completedTasks }}
-                            </span>
-                        </v-fade-transition>
-                    </v-col-3>
-                    <v-spacer/>
-                        <v-progress-circular v-show="tasks.length > 0" class="me-3" color="success" :model-value="(completedTasks / tasks.length) * 100 ">
-                        </v-progress-circular>
-                </v-row>
-                <v-divider class="mt-5"/>
-
-                <v-card class="mt-4" v-show="tasks.length > 0">
-
-                    <v-slide-y-transition group>
-                                <div v-for="task in tasks" :key="task.task_id">
-                                    <v-hover v-slot="{isHovering, props}">
-                                        <v-divider/>
-                                        <v-list-item :class="isHovering ? 'bg-grey-lighten-3' : ''" v-bind="props">
-                                                    <v-checkbox-btn @click="completeTask(task)" :label="task.name" :class="task.is_completed ? 'text-grey' : 'text-black'">
-                                                    </v-checkbox-btn>
-                                            <template v-slot:append>
-                                                <v-fade-transition>
-                                                    <v-icon color="success" v-show="task.is_completed"> mdi-check </v-icon>
-                                                </v-fade-transition>
-                                            </template>
-                                        </v-list-item>
-                                    </v-hover>
-                                </div>
-                    </v-slide-y-transition>
-
-                </v-card>
-
             </v-container>
+
         </v-main>
     </v-app>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: '/todo-list/`',
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
